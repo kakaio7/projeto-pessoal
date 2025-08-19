5384d94861cdde75a178917fab5e2a8e0e93d66c
Kaua Gomes <kaua.bezerra7@aluno.ce.gov.br>
2025-08-19 16:51:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+let pergunta = prompt("Escolha sua raça")
+
+if(pergunta == "saiyajin"){
+    alert(`
+        Origem: Planeta Vegeta, com alta gravidade
+        
+       Aparência: Variável, mas geralmente possuem cabelos negros e olhos escuros.
+       
+       Habilidades: Super força, velocidade, capacidade de voar e se transformar em Super Saiyajin e outras formas evoluídas.
+       
+       Longevidade: Vida útil semelhante aos humanos mas com pico de desempenho até os 80 anos.
+       
+       Personalidade: Muitos Saiyajins são conhecidos por sua arrogância e sede de batalha, sentindo-se superiores a outras raças.
+       
+       Transformações: Super Saiyajin,Super Saiyajin 2, Super Saiyajin 3, Super Saiyajin God, Super Saiyajin Blue, Super Saiyajin Rose, Super Saiyajin Blue Evolution, Istinto Superior Incompleto, Istinto Superior Completo.
+       
+       Importante!
+       
+       Híbridos: Saiyajins com sangue humano, como Gohan, Goten, Trunks, e Bra, também podem se transformar em Super Saiyajin, mas alguns fãs especulam sobre seu potencial máximo comparado a Saiyajins de sangue puro.
+       
+       `)
+}
+else if(pergunta == "namekuseijin") {
+    alert(`
+      Força e Constituição: Namekuseijins geralmente começam com força e constituição mais baixas em comparação com outras raças, como Saiyajins
+      
+      Poder de Ki: Eles possuem um aumento base de 30% em Poder de Ki e uma Reserva de Ki 20% maior, o que lhes confere vantagens em combate. 
+      
+      Longevidade: Namekuseijins tendem a viver muito mais tempo do que os humanos. 
+
+      Habilidades Especiais: Além da regeneração, alguns Namekuseijins podem usar habilidades mágicas como a criação das Esferas do Dragão e outras habilidades relacionadas à cura. 
+
+      Tamanho: Em média, os Namekuseijins têm entre 1,50 e 2 metros de altura. 
+
+      Planeta: A raça se originou em Namekusei e, em algumas histórias, foram encontrados em outros planetas como Nova Namekusei e a Terra. 
+
+        `)
+}
+else if(pergunta == "majin"){
+    alert(`
+      Aparência: Variação de cores de pele (vermelho, azul claro, rosa, roxo, lavanda, cinza, branco pálido), orelhas pontudas e tentáculos no lugar do cabelo.
+        
+     Longevidade: Apesar de não serem explicitamente imortais, Majins envelhecem muito lentamente e possuem grande resistência. 
+     
+     Poderes: Regeneração, absorção de energia e de outros seres, manipulação da matéria (podem se transformar em vários objetos) e técnicas de cura. 
+     
+     Regeneração: A habilidade de regenerar partes do corpo perdidas é uma das principais características dos Majins, tornando-os difíceis de serem derrotados. 
+     
+     Absorção: Majins podem absorver outros seres, ganhando seus poderes e características.`)
+}
+else if(pergunta == "Shin-jin"){
+    alert(`
+    Origem: Os Shin-jin nascem no planeta Kaishin, cuja localização exata no universo é desconhecida. 
+
+    Poder e Habilidades
+
+    Força: Possuem uma força natural impressionante, com os Kaios tendo níveis de poder altos e os Kaioshins sendo ainda mais fortes. 
+
+    Magia: Demonstram afinidade com magia, como visto em Zamasu, que a utiliza de forma eficaz. 
+
+    Habilidades Especiais: Além da força física, os Shin-jin podem desenvolver habilidades únicas, como as técnicas de teletransporte e cura dos Kaios.
+    
+   `)
+}
