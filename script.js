ad44fb681916b2f322cd12daf744adaceb4fb35e
Kaua Gomes <kaua.bezerra7@aluno.ce.gov.br>
2025-08-26 12:50:14 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,66 +1,135 @@
-let pergunta = prompt("Escolha sua raça")
+let pergunta = prompt(`Escolha sua raça: 
+saiyajin
+namekuseijin
+majin
+shin-jin
+anjo
+arcosiano
+androide
+humano`)
 
 if(pergunta == "saiyajin"){
-    alert(`
-        Origem: Planeta Vegeta, com alta gravidade
+   alert(`
+  Origem: Planeta Vegeta, com alta gravidade
         
-       Aparência: Variável, mas geralmente possuem cabelos negros e olhos escuros.
+  Aparência: Variável, mas geralmente possuem cabelos negros e olhos escuros.
        
-       Habilidades: Super força, velocidade, capacidade de voar e se transformar em Super Saiyajin e outras formas evoluídas.
+  Habilidades: Super força, velocidade, capacidade de voar e se transformar em Super Saiyajin e outras formas evoluídas.
        
-       Longevidade: Vida útil semelhante aos humanos mas com pico de desempenho até os 80 anos.
+  Longevidade: Vida útil semelhante aos humanos mas com pico de desempenho até os 80 anos.
        
-       Personalidade: Muitos Saiyajins são conhecidos por sua arrogância e sede de batalha, sentindo-se superiores a outras raças.
+  Personalidade: Muitos Saiyajins são conhecidos por sua arrogância e sede de batalha, sentindo-se superiores a outras raças.
        
-       Transformações: Super Saiyajin,Super Saiyajin 2, Super Saiyajin 3, Super Saiyajin God, Super Saiyajin Blue, Super Saiyajin Rose, Super Saiyajin Blue Evolution, Instinto Superior Incompleto, Instinto Superior Completo.
+  Transformações: Super Saiyajin,Super Saiyajin 2, Super Saiyajin 3, Super Saiyajin God, Super Saiyajin Blue, Super Saiyajin Rose, Super Saiyajin Blue Evolution, Instinto Superior Incompleto, Instinto Superior Completo.
        
-       Importante!
+  Importante!
        
-       Híbridos: Saiyajins com sangue humano, como Gohan, Goten, Trunks, e Bra, também podem se transformar em Super Saiyajin, mas alguns fãs especulam sobre seu potencial máximo comparado a Saiyajins de sangue puro.
+  Híbridos: Saiyajins com sangue humano, como Gohan, Goten, Trunks, e Bra, também podem se transformar em Super Saiyajin, mas alguns fãs especulam sobre seu potencial máximo comparado a Saiyajins de sangue puro.
        
-       `)
+  `)
 }
 else if(pergunta == "namekuseijin") {
-    alert(`
-      Força e Constituição: Namekuseijins geralmente começam com força e constituição mais baixas em comparação com outras raças, como Saiyajins
+  alert(`
+  Força e Constituição: Namekuseijins geralmente começam com força e constituição mais baixas em comparação com outras raças, como Saiyajins
       
-      Poder de Ki: Eles possuem um aumento base de 30% em Poder de Ki e uma Reserva de Ki 20% maior, o que lhes confere vantagens em combate. 
+  Poder de Ki: Eles possuem um aumento base de 30% em Poder de Ki e uma Reserva de Ki 20% maior, o que lhes confere vantagens em combate. 
       
-      Longevidade: Namekuseijins tendem a viver muito mais tempo do que os humanos. 
+  Longevidade: Namekuseijins tendem a viver muito mais tempo do que os humanos. 
 
-      Habilidades Especiais: Além da regeneração, alguns Namekuseijins podem usar habilidades mágicas como a criação das Esferas do Dragão e outras habilidades relacionadas à cura. 
+  Habilidades Especiais: Além da regeneração, alguns Namekuseijins podem usar habilidades mágicas como a criação das Esferas do Dragão e outras habilidades relacionadas à cura. 
 
-      Tamanho: Em média, os Namekuseijins têm entre 1,50 e 2 metros de altura. 
+  Tamanho: Em média, os Namekuseijins têm entre 1,50 e 2 metros de altura. 
 
-      Planeta: A raça se originou em Namekusei e, em algumas histórias, foram encontrados em outros planetas como Nova Namekusei e a Terra. 
+  Planeta: A raça se originou em Namekusei e, em algumas histórias, foram encontrados em outros planetas como Nova Namekusei e a Terra. 
 
-        `)
+  `)
 }
 else if(pergunta == "majin"){
-    alert(`
-      Aparência: Variação de cores de pele (vermelho, azul claro, rosa, roxo, lavanda, cinza, branco pálido), orelhas pontudas e tentáculos no lugar do cabelo.
+  alert(`
+  Aparência: Variação de cores de pele (vermelho, azul claro, rosa, roxo, lavanda, cinza, branco pálido), orelhas pontudas e tentáculos no lugar do cabelo.
         
-     Longevidade: Apesar de não serem explicitamente imortais, Majins envelhecem muito lentamente e possuem grande resistência. 
+  Longevidade: Apesar de não serem explicitamente imortais, Majins envelhecem muito lentamente e possuem grande resistência. 
      
-     Poderes: Regeneração, absorção de energia e de outros seres, manipulação da matéria (podem se transformar em vários objetos) e técnicas de cura. 
+  Poderes: Regeneração, absorção de energia e de outros seres, manipulação da matéria (podem se transformar em vários objetos) e técnicas de cura. 
      
-     Regeneração: A habilidade de regenerar partes do corpo perdidas é uma das principais características dos Majins, tornando-os difíceis de serem derrotados. 
+  Regeneração: A habilidade de regenerar partes do corpo perdidas é uma das principais características dos Majins, tornando-os difíceis de serem derrotados. 
      
-     Absorção: Majins podem absorver outros seres, ganhando seus poderes e características.`)
+  Absorção: Majins podem absorver outros seres, ganhando seus poderes e características.`)
 }
-else if(pergunta == "Shin-jin"){
-    alert(`
-    Origem: Os Shin-jin nascem no planeta Kaishin, cuja localização exata no universo é desconhecida. 
+else if(pergunta == "shin-jin"){
+  alert(`
+  Origem: Os Shin-jin nascem no planeta Kaishin, cuja localização exata no universo é desconhecida. 
 
-    Poder e Habilidades
+  Poder e Habilidades
 
-    Força: Possuem uma força natural impressionante, com os Kaios tendo níveis de poder altos e os Kaioshins sendo ainda mais fortes. 
+  Força: Possuem uma força natural impressionante, com os Kaios tendo níveis de poder altos e os Kaioshins sendo ainda mais fortes. 
 
-    Magia: Demonstram afinidade com magia, como visto em Zamasu, que a utiliza de forma eficaz. 
+  Magia: Demonstram afinidade com magia, como visto em Zamasu, que a utiliza de forma eficaz. 
 
-    Habilidades Especiais: Além da força física, os Shin-jin podem desenvolver habilidades únicas, como as técnicas de teletransporte e cura dos Kaios.
+  Habilidades Especiais: Além da força física, os Shin-jin podem desenvolver habilidades únicas, como as técnicas de teletransporte e cura dos Kaios.
     
-   `)
+  `)
 }
+else if(pergunta == "anjo"){
+  alert(`
+  Poder e Habilidades
 
-      
+  Força Superior: Anjos possuem uma força que supera a dos deuses da destruição, sendo que o mais forte, Daishinkan, é considerado o guerreiro mais forte do multiverso. 
+
+  Controle Temporal: Anjos podem manipular o tempo, revertendo danos causados por deuses da destruição ou utilizando-o para fins diversos. 
+
+  Habilidades Extras: Além do controle temporal, anjos podem se teletransportar, materializar objetos, prever o futuro, curar, ressuscitar e manipular a matéria. 
+
+  Instinto Superior: Anjos possuem o Instinto Superior completo e ativo, o que lhes permite se esquivar de ataques sem esforço. 
+
+  Imortalidade: Anjos são imortais, mas podem ser eliminados se violarem as leis dos anjos, como lutar seriamente contra mortais.  
+
+  Características Físicas e Sociais 
+
+  Limitações: Anjos não podem usar seus poderes livremente em combate e possuem limitações em suas habilidades de ressuscitação. 
+  `)
+}
+else if(pergunta == "arcosiano"){
+  alert(`
+  Estatísticas e Características:
+
+  Atributos Iniciais: Arcosianos começam com 15 em Espírito e Constituição, mas têm Destreza e Mente em 5. 
+
+  Modificadores: Recebem um bônus de 20% em Defesa, 10% em Vigor e 30% em Velocidade. 
+
+  Transformações: Arcosianos podem se transformar para aumentar seu poder, com multiplicadores que variam de 2x (segunda forma) a 15x (forma de aumento). 
+
+  Potencial de Poder: São considerados uma raça com alto potencial de se tornar mais forte. 
+
+  Base de Poder: Geralmente começam mais fortes do que outras raças. 
+  `)
+}
+else if(pergunta == "androide"){
+  alert(`
+  Características gerais dos androides
+
+  Tipos de Androides: Existem androides totalmente artificiais (como o 8 e o 16), androides com absorção de energia (como o 19 e o 20) e bio-androides (como o Cell).
+
+  Poder e Habilidades: A força e as habilidades dos androides variam muito. Alguns, como o 17 e o 18, possuem força e velocidade incríveis, enquanto outros, como o 16, são notáveis por sua resistência.
+
+  Imunidade ao Ki: Alguns androides, especialmente os totalmente artificiais, são imunes à detecção por rastreadores de Ki e podem até mesmo suprimir seu próprio Ki.
+
+  Fontes de Energia: Alguns androides, como o 19 e o 20, precisam absorver energia para se manterem funcionando, enquanto outros, como o 17 e o 18, têm fontes de energia ilimitadas.
+
+  Criação e Evolução: O Dr. Gero continuou a desenvolver androides com diferentes propósitos e níveis de poder ao longo da série, culminando em androides cada vez mais poderosos e complexos. 
+
+  `)
+}else if(pergunta == "humano"){
+  alert(`
+  Curiosidades em Destaque: Estatísticas dos Humanos:
+
+  Estatísticas Gerais dos humanos: Humanos treinados como Mestre Kame, Krillin, Tien e Yamcha possuem.
+
+  Força: Capazes de quebrar rochas ou levantar objetos pesados com facilidade.
+
+  Velocidade: Superam a velocidade do som; Krillin chegou a voar mais rápido que o Jato de Bulma (~Mach 1.5)
+
+  Durabilidade: Resistência a balas e golpes/rajadas de alienígenas poderosos, até com ki "divinos"
+
+  `)
+}
